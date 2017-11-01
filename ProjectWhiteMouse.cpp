c42210c1e862ad59b9d9d900b9c8fe7618bbc08b
borisgalicky <boris.galicky@akademiasovy.sk>
2017-11-01 19:40:39 +0100

    added isNumberPalindrom

--- ProjectWhiteMouse.cpp
+++ ProjectWhiteMouse.cpp
@@ -273,4 +273,70 @@ float amount;
 vat = amount/20%;
 return result;
 }
+
+/* 
+* Finding out whether number is a palindrom
+* Author: Boris Galickı
+* Date: 31-10-2017
+* @param: number
+* @return: result
+*/
+int isNumberPalindrom(int number)
+{
+int a,b,c,d;
+if(number<100)
+	{
+	a=(number%10);
+	b=(number/10);
+		if(a==b)
+		{
+		printf("YES");
+		return 1;
+		}
+		else
+		{
+		printf("NO");
+		return 0;
+		}
+	}
+else if((number>=100) and (number<1000))
+	{
+	a=(number%10);
+	b=(number/100);
+		if(a==b)
+		{
+		printf("YES");
+		return 1;
+		}
+		else
+		{
+		printf("NO");
+		return 0;
+		}	
+	}
+else if((number>=1000) and (number<10000))
+	{
+	a=(number%10);
+	number=(number/10);
+	b=(number%10);
+	number=(number/10);
+	c=(number%10);
+	number=(number/10);
+	d=(number%10);
+    	if((a==d) and (b==c))
+    	{
+    	printf("YES");
+    	return 1;
+		}
+		else
+		{
+		printf("NO");
+		return 0;
+		}
+	}
+else
+	{
+	printf("WRONG INPUT!");
+	}
+}
 		
